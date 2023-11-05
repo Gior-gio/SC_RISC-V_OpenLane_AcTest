72bd7fa41ad4ad1fd9f87f716c793b9c8d156a3f
Jorge Angarita Pérez <68038923+Gior-gio@users.noreply.github.com>
2023-11-04 22:19:15 -0500

    Add new module definition
    for TT compatibility issues

--- src/tt_um_src.v
+++ src/tt_um_src.v
@@ -1,10 +1,24 @@
 module tt_um_src(
-    input clk,
-    input reset,
-    input [4:0] sw,
-    input IR,
-    output [3:0] led
-);
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to 
+    
+    );
+
+//Connection with old variables
+wire reset = ~rst_n;
+wire [4:0] sw = ui_in[4:0];
+wire IR = ui_in[5];
+wire [3:0] led = uo_out[3:0];
+assign uo_out[7:4] = 3'b000;
+assign uio_oe = 8'b11111000;
+assign uio_out[7:0] = 8'b00000000;
+    
 wire Zero;
 //Program Counter and Related:
 reg [4:0] PC;
